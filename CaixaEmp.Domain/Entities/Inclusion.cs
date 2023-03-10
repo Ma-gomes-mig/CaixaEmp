ecbca46d12309142d192ab84ee90b10ff9ae5f72
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-09 21:01:48 -0300

    Criação da classe Inclusion

---
+++ CaixaEmp.Domain/Entities/Inclusion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Domain.Entities
+{
+    public sealed class Inclusion : Base
+    {
+        //Atributos Inclusion
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int Value { get; private set; }
+        public DateTime DateInclusion { get; private set; }
+        public string Priority { get; private set; }
+        public string Status { get; private set; }
+
+        //Construtor vazio
+        public Inclusion()
+        {
+
+        }
+
+        //Construtor com todas as propriedades
+        public Inclusion(int id, string name, string description, int value, DateTime dateInclusion, string priority, string status)
+        {
+            Id = id;
+            ValidateName(name);
+            Description = description;
+            Value = value;
+            DateInclusion = dateInclusion;
+            Priority = priority;
+            Status = status;
+        }
+
+        //Construtor para Update
+        public void Update(string name, string description, DateTime dateInclusion, string priority, string status)
+        {
+            ValidateName(name);
+            Description = description;
+            DateInclusion = dateInclusion;
+            Priority = priority;
+            Status = status;
+        }
+
+        //Definição da cardinalidade N Withdrawal para 1 Emplooyer
+        public int EmplooyerId { get; set; }
+        public Emplooyer Emplooyer { get; set; }
+
+        //Definição da cardinalidade N Withdrawal para 1 Expense
+        public int ExpenseId { get; set; }
+        public Expense Expense { get; set; }
+
+        //Fazendo validação Se nome null msg erro
+        private void ValidateName(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "O nome é necessário");
+            Name = name;
+        }
+    }
+}
