4860b73eb1f32c205b943409b013ded5f5e93613
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-09 21:08:43 -0300

    Criação da interface IExpenseRepository

---
+++ CaixaEmp.Domain/Interface/IExpenseRepository.cs
@@ -0,0 +1,21 @@
+﻿using CaixaEmp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Domain.Interface
+{
+    public interface IExpenseRepository
+    {
+        Task<IEnumerable<Expense>> GetAllExpenseAsync();
+        Task<Expense> GetExpenseById(int? id);
+        Task<IEnumerable<Expense>> GetExpenseByEmplooyer(int emplooyerId);
+        Task<IEnumerable<Expense>> GetExpenseByCategory(int categoryId);
+
+        Task<Expense> Create(Expense expense);
+        Task<Expense> Update(Expense expense);
+        Task<Expense> Delete(int id);
+    }
+}
