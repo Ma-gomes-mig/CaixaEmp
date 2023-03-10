aacee296f11522e122f1bb883d65a1acc20c2695
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-09 21:11:20 -0300

    Criação da interface IWithdrawal

---
+++ CaixaEmp.Domain/Interface/IWithdrawalRepository.cs
@@ -0,0 +1,25 @@
+﻿using CaixaEmp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Domain.Interface
+{
+    public interface IWithdrawalRepository
+    {
+        Task<IEnumerable<Withdrawal>> GetAllWithdrawalAsync();
+        Task<Withdrawal> GetWithdrawalById(int? id);
+        Task<IEnumerable<Withdrawal>> GetWithdrawalByExpense(int expenseId);
+        Task<IEnumerable<Withdrawal>> GetWithdrawalByEmplooyer(int emplooyerId);
+        Task<IEnumerable<Withdrawal>> GetWithdrawalByPeriod(DateTime beginDate, DateTime endDate);
+        Task<IEnumerable<Withdrawal>> GetWithdrawalByPriority(Withdrawal priority);
+        Task<IEnumerable<Withdrawal>> GetWithdrawalAproved(Withdrawal status);
+        Task<IEnumerable<Withdrawal>> GetInclusionReproved(Withdrawal status);
+
+        Task<Withdrawal> Create(Withdrawal withdrawal);
+        Task<Withdrawal> Update(Withdrawal withdrawal);
+        Task<Withdrawal> Delete(int id);
+    }
+}
