9b3e340f6295a14e13724149086b64c44f4b4972
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-09 21:05:54 -0300

    Criação da Interface IEmplooyerRepository

---
+++ CaixaEmp.Domain/Interface/IEmplooyerRepository.cs
@@ -0,0 +1,24 @@
+﻿using CaixaEmp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Domain.Interface
+{
+    public interface IEmplooyerRepository
+    {
+        Task<IEnumerable<Emplooyer>> GetAllEmplooyerAsync();
+        Task<Emplooyer> GetEmplooyerByIdAsync(int? id);
+        Task<Emplooyer> GetEmplooyerByName(string name);
+        Task<IEnumerable<Emplooyer>> GetEmplooyerByExpense(int expenseId);
+
+        Task<Emplooyer> Create(Emplooyer emplooyer);
+        Task<Emplooyer> Update(Emplooyer emplooyer);
+        Task<Emplooyer> Delete(int id);
+    }
+    internal interface IEmplooyerRepository
+    {
+    }
+}
