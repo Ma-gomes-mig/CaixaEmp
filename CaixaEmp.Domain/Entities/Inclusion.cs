71e13cd599271e43064a95ec2b2eb6c9e05c090b
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-09 22:03:14 -0300

    Criação ApplicationDbContext

--- CaixaEmp.Domain/Entities/Inclusion.cs
+++ CaixaEmp.Domain/Entities/Inclusion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CaixaEmp.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ CaixaEmp.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CaixaEmp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> context) : base(context) { }
+
+        public DbSet<Emplooyer> Emplooyers { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<ExpenseCategory> Categories { get; set; }
+        public DbSet<Inclusion> Inclusions { get; set; }
+        public DbSet<Withdrawal> Withdrawals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
