57df6c1ecb4b46c27fc5f7545e4ea175ea6522c1
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-10 21:19:49 -0300

    Criação test CreateEmplooyerWithValidTest

---
+++ CaixaEmp.DomainTest/EmplooyerUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using CaixaEmp.Domain.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.DomainTest
+{
+    public class EmplooyerUnitTest
+    {
+        [Fact(DisplayName ="CreateEmplooyerWithValidTest")]
+        public void CreateEmplooyer_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Emplooyer(1, "EmplooyerName","EmplooyerFunction");
+            action.Should()
+                .NotThrow<CaixaEmp.Domain.Validation.DomainExceptionValidation>();
+        }
+    }
+}
