c06cc2a9688ec054094535547290da192126b20a
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-12 19:55:51 -0300

    Criação da classe InclusionConfiguration

---
+++ CaixaEmp.Infra.Data/EntitiesConfiguration/InclusionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CaixaEmp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.EntitiesConfiguration
+{
+    public class InclusionConfiguration : IEntityTypeConfiguration<Inclusion>
+    {
+
+    }
+}
