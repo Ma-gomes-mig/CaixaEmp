f52546c2dcedc265e1336d0a0d4311c7ad5b72cc
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-12 19:50:36 -0300

    Criação da classe ExpenseCategoryConfiguration

---
+++ CaixaEmp.Infra.Data/EntitiesConfiguration/ExpenseCategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CaixaEmp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.EntitiesConfiguration
+{
+    public class ExpenseCategoryConfiguration : IEntityTypeConfiguration<ExpenseCategory>
+    {
+        public void Configure(EntityTypeBuilder<ExpenseCategory> builder)
+        {
+            builder.HasKey(k => k.Id);
+            builder.Property(p => p.Name).HasMaxLength(30).IsRequired();
+            builder.Property(p=>p.Description).HasMaxLength(150).IsRequired();
+
+            
+        }
+    }
+}
