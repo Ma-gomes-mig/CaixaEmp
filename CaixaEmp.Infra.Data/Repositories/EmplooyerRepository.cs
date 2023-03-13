edfe6ca64f9fa5018a6016cfafb0e3eee9af03c1
Matheus <matheus.gom23@yahoo.com>
2023-03-13 14:56:18 -0300

    Criação da classe EmplooyerRepository

---
+++ CaixaEmp.Infra.Data/Repositories/EmplooyerRepository.cs
@@ -0,0 +1,20 @@
+﻿using CaixaEmp.Domain.Entities;
+using CaixaEmp.Domain.Interface;
+using CaixaEmp.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.Repositories
+{
+    public class EmplooyerRepository : IEmplooyerRepository
+    {
+        ApplicationDbContext _emplooyerContext;
+        public EmplooyerRepository(ApplicationDbContext context)
+        {
+            _emplooyerContext = context;
+        }
+    }
+}
