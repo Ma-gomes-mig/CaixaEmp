c26746ca78e5e60c55350b73d59451e2bc036da4
Matheus <matheus.gom23@yahoo.com>
2023-03-13 16:56:26 -0300

    Declaração do método Create

--- CaixaEmp.Infra.Data/Repositories/ExpenseRepository.cs
+++ CaixaEmp.Infra.Data/Repositories/ExpenseRepository.cs
@@ -1,4 +1,5 @@
-﻿using CaixaEmp.Domain.Interface;
+﻿using CaixaEmp.Domain.Entities;
+using CaixaEmp.Domain.Interface;
 using CaixaEmp.Infra.Data.Context;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,42 @@ namespace CaixaEmp.Infra.Data.Repositories
         public ExpenseRepository(ApplicationDbContext context)
         {
             _expenseContext = context;
+        }        
+
+        public Task<IEnumerable<Expense>> GetAllExpenseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Expense> GetExpenseById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<Expense>> GetExpenseByEmplooyer(int emplooyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Expense>> GetExpenseByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }      
+
+        public async Task<Expense> Create(Expense expense)
+        {
+            _expenseContext.Add(expense);
+            await _expenseContext.SaveChangesAsync();
+            return expense;
+        }
+
+        public Task<Expense> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Expense> Update(Expense expense)
+        {
+            throw new NotImplementedException();
         }
     }
 }
