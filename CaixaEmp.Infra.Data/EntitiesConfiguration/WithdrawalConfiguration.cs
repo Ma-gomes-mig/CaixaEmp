b8bd76ff01dc9eb0d24191e78ea26b4a1d0163e9
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-12 21:14:02 -0300

    Criação da classe WithdrawalConfiguration

---
+++ CaixaEmp.Infra.Data/EntitiesConfiguration/WithdrawalConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using CaixaEmp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.EntitiesConfiguration
+{
+    public class WithdrawalConfiguration : IEntityTypeConfiguration<Withdrawal>
+    {
+        public void Configure(EntityTypeBuilder<Withdrawal> builder)
+        {
+            builder.HasKey(k => k.Id);
+            builder.Property(p => p.Name).HasMaxLength(50);
+            builder.Property(p => p.Description).HasMaxLength(150);
+            builder.Property(p => p.Value).HasPrecision(10, 2).IsRequired();
+            builder.Property(p => p.DateInclusion).IsRequired();
+            builder.Property(p => p.Priority).HasMaxLength(30);
+            builder.Property(p => p.Status).HasMaxLength(30);
+        }
+    }
+}
