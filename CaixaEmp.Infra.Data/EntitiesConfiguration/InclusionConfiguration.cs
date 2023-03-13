08a0ff2bcda27b62035f0775b2a8a1a0ffa89258
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-12 21:10:42 -0300

    Criação da classe InclusionConfiguration

--- CaixaEmp.Infra.Data/EntitiesConfiguration/InclusionConfiguration.cs
+++ CaixaEmp.Infra.Data/EntitiesConfiguration/InclusionConfiguration.cs
@@ -16,7 +16,13 @@ namespace CaixaEmp.Infra.Data.EntitiesConfiguration
             builder.HasKey(k => k.Id);
             builder.Property(p => p.Name).HasMaxLength(50);
             builder.Property(p => p.Description).HasMaxLength(150);
-            builder.Property(p => p.)
+            builder.Property(p => p.Value).HasPrecision(10, 2).IsRequired();    
+            builder.Property(p =>p.DateInclusion).IsRequired();
+            builder.Property(p => p.Priority).HasMaxLength(30);
+            builder.Property(p => p.Status).HasMaxLength(30);
+
+            builder.HasOne(e => e.Emplooyer).WithMany(e => e.Inclusions).HasForeignKey(f => f.EmplooyerId);
+            builder.HasOne(e => e.Expense).WithMany(e => e.Inclusions).HasForeignKey(f => f.ExpenseId);
         }
     }
 }
