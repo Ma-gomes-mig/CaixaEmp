2b09c8ed091ff410481d3b5b7ca105b806050611
Matheus <matheus.gom23@yahoo.com>
2023-03-14 12:25:07 -0300

    Declaração dos métodos da Interface IInclusionRepository

--- CaixaEmp.Infra.Data/Repositories/InclusionRepository.cs
+++ CaixaEmp.Infra.Data/Repositories/InclusionRepository.cs
@@ -1,4 +1,5 @@
-﻿using CaixaEmp.Domain.Interface;
+﻿using CaixaEmp.Domain.Entities;
+using CaixaEmp.Domain.Interface;
 using CaixaEmp.Infra.Data.Context;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,61 @@ namespace CaixaEmp.Infra.Data.Repositories
         public InclusionRepository(ApplicationDbContext context)
         {
             _inclusionContext = context;
+        }       
+
+        public Task<IEnumerable<Inclusion>> GetAllInclusionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Inclusion>> GetInclusionAproved(Inclusion status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Inclusion>> GetInclusionByEmplooyer(int emplooyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Inclusion>> GetInclusionByExpense(int expenseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Inclusion> GetInclusionById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Inclusion>> GetInclusionByPeriod(DateTime beginDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Inclusion>> GetInclusionByPriority(Inclusion priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Inclusion>> GetInclusionReproved(Inclusion status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Inclusion> Create(Inclusion inclusion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Inclusion> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Inclusion> Update(Inclusion inclusion)
+        {
+            throw new NotImplementedException();
         }
     }
 }
