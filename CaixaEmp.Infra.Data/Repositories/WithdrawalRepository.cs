27f27884089bca1b1acaa37f697563b6f2af46c5
Matheus <matheus.gom23@yahoo.com>
2023-03-15 15:06:46 -0300

    Criação da classe WithdrawalRepoditory

---
+++ CaixaEmp.Infra.Data/Repositories/WithdrawalRepository.cs
@@ -0,0 +1,19 @@
+﻿using CaixaEmp.Domain.Interface;
+using CaixaEmp.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.Repositories
+{
+    public class WithdrawalRepository : IWithdrawalRepository
+    {
+        ApplicationDbContext _withdrawalContext;
+        public WithdrawalRepository(ApplicationDbContext context)
+        {
+            _withdrawalContext = context;
+        }
+    }
+}
