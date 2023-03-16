34f39ac9147616c6020288b53e6ebac10516d197
Matheus <matheus.gom23@yahoo.com>
2023-03-16 17:49:35 -0300

    Método GetInclusionAproved Foreach testado

--- CaixaEmp.Infra.Data/Repositories/InclusionRepository.cs
+++ CaixaEmp.Infra.Data/Repositories/InclusionRepository.cs
@@ -53,10 +53,10 @@ namespace CaixaEmp.Infra.Data.Repositories
             var query = from i in _inclusionContext.Inclusions
                         where i.Status == true
                         select i;
-            //foreach(var itens in query)
-            //{
-            //    Console.WriteLine(itens);
-            //}
+            foreach (var itens in query)
+            {
+                Console.WriteLine(itens);
+            }
 
             return await query.ToListAsync();
         }
