e70411edad3a388789f0c2b2851bb03dfe11cf41
Matheus <matheus.gom23@yahoo.com>
2023-03-18 11:34:14 -0300

    Deleção de um método na classe EmplooyerRepository e na interface IEmplooyerRepository

--- CaixaEmp.Domain/Interface/IEmplooyerRepository.cs
+++ CaixaEmp.Domain/Interface/IEmplooyerRepository.cs
@@ -12,8 +12,6 @@ namespace CaixaEmp.Domain.Interface
         Task<IEnumerable<Emplooyer>> GetAllEmplooyerAsync();
         Task<Emplooyer> GetEmplooyerByIdAsync(int? id);
         Task<Emplooyer> GetEmplooyerByName(string name);
-
-        //Task<IEnumerable<Emplooyer>> GetEmplooyerByExpense(Expense expense);
         Task<Emplooyer> Create(Emplooyer emplooyer);
         Task<Emplooyer> Update(Emplooyer emplooyer);
         Task<Emplooyer> Delete(Emplooyer emplooyer);
