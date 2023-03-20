a7446eee3617d19ed2c950ee7a1151aaa4e4b00d
Matheus <matheus.gom23@yahoo.com>
2023-03-20 15:44:58 -0300

    Desclaração do ExpenseDTO

--- CaixaEmp.Application/DTOs/ExpenseDTO.cs
+++ CaixaEmp.Application/DTOs/ExpenseDTO.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,35 @@ namespace CaixaEmp.Application.DTOs
 {
     public class ExpenseDTO
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName]
+        public string Name { get; set; }
+
+        [StringLength(150)]
+        [DisplayName("Descrição")]
+        public string Description { get; set; }
+
+       public decimal Value { get; set; }
+        [Required]
+        [StringLength(30)]
+       public string Priority { get; set; }
+
+        //Definição da cardinalidade N Expense para 1 Emplooyer
+        public int EmplooyerId { get; set; }
+        public Emplooyer Emplooyer { get; set; }
+
+        //Definição da cardinalidade N Expense para 1 ExpenseCategory
+        public int ExpenseCategoryId { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+
+        //Definição da cardinalidade N withdrawal para 1 Expense
+        public ICollection<Withdrawal> Withdrawals { get; set; }
+
+        //Definição da cardinalidade N Inclusion para 1 Expense
+        public ICollection<Inclusion> Inclusions { get; set; }       
     }
 }
--- CaixaEmp.Infra.Data/EntitiesConfiguration/ExpenseConfiguration.cs
+++ CaixaEmp.Infra.Data/EntitiesConfiguration/ExpenseConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using CaixaEmp.Domain.Entities;
+﻿        using CaixaEmp.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
