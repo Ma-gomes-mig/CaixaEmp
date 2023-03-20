bd1d8e7e38d20596afea33a178d8cd15831071c0
Matheus <matheus.gom23@yahoo.com>
2023-03-20 16:06:20 -0300

    Declaração da classe InclusionDTO

--- CaixaEmp.Application/DTOs/InclusionDTO.cs
+++ CaixaEmp.Application/DTOs/InclusionDTO.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,38 @@ namespace CaixaEmp.Application.DTOs
 {
     public class InclusionDTO
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Description { get; set; }
+        [Required]
+        [DisplayName("Valor")]
+        public decimal Value { get; set; }
+
+        [DisplayName("Data")]
+        public DateTime DateInclusion { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Prioridade")]
+        public string Priority { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Estatus")]
+        public string Status { get; set; }
+
+        //Definição da cardinalidade N Inclusion para 1 Emplooyer
+        public int EmplooyerId { get; set; }
+        public Emplooyer Emplooyer { get; set; }
+
+        //Definição da cardinalidade N Inclusion para 1 Expense
+        public int ExpenseId { get; set; }
+        public Expense Expense { get; set; }
+
+        //Definição da cardinalidade N Inclusion para 1 Balance
+        public int BalanceId { get; set; }
+        public Balance Balance { get; set; }
     }
 }
--- CaixaEmp.Infra.Data/EntitiesConfiguration/InclusionConfiguration.cs
+++ CaixaEmp.Infra.Data/EntitiesConfiguration/InclusionConfiguration.cs
@@ -14,7 +14,7 @@ namespace CaixaEmp.Infra.Data.EntitiesConfiguration
         public void Configure(EntityTypeBuilder<Inclusion> builder)
         {
             builder.HasKey(k => k.Id);
-            builder.Property(p => p.Name).HasMaxLength(50);
+            //builder.Property(p => p.Name).HasMaxLength(50);
             builder.Property(p => p.Description).HasMaxLength(150);
             builder.Property(p => p.Value).HasPrecision(10, 2).IsRequired();    
             builder.Property(p =>p.DateInclusion).IsRequired();
