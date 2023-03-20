2d33369ea4434caedb9209df7b2ab960a052debd
Matheus <matheus.gom23@yahoo.com>
2023-03-20 10:54:32 -0300

    Declaração da classe BalanceDTO

--- CaixaEmp.Application/DTOs/BalanceDTO.cs
+++ CaixaEmp.Application/DTOs/BalanceDTO.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,15 @@ namespace CaixaEmp.Application.DTOs
 {
     public class BalanceDTO
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Valor")]
+        public decimal Value { get; set; }
+
+        public ICollection<Inclusion> Inclusions { get; set; }
+        public ICollection<Withdrawal> Withdrawals { get; set; }
+
     }
 }
