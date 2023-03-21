16a19e79a38d5039cc5fe5ec6bae046db9679474
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-21 20:20:37 -0300

    Merge

---
+++ CaixaEmp.Infra.Data/Repositories/BalanceRespository.cs
@@ -0,0 +1,26 @@
+﻿using CaixaEmp.Domain.Entities;
+using CaixaEmp.Domain.Interfaces;
+using CaixaEmp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Infra.Data.Repositories
+{
+    public class BalanceRespository : IBalanceRepository
+    {
+        ApplicationDbContext _applicationDbContext;
+        public BalanceRespository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IEnumerable<Balance>> GetAllBalanceAsync()
+        {
+            return await _applicationDbContext.Balances.ToListAsync();
+        }
+    }
+}
