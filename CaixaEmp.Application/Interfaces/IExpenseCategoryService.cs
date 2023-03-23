3d3bed8e03eeffeff3208297fa2fb4cffaf0d726
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-23 20:51:13 -0300

    Implementação da interface IEmplooyerService

--- CaixaEmp.Application/Interfaces/IExpenseCategoryService.cs
+++ CaixaEmp.Application/Interfaces/IExpenseCategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CaixaEmp.Application.DTOs;
+using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,11 @@ namespace CaixaEmp.Application.Interfaces
 {
     public interface IExpenseCategoryService
     {
-     
+        Task<IEnumerable<ExpenseCategoryDTO>> GetAllCategoryAsync();
+        Task<IEnumerable<ExpenseCategoryDTO>> GetCategoryByExpense(int expenseId);
+        Task<ExpenseCategoryDTO> GetCategoryByIdAsync(int? Id);
+        Task Create(ExpenseCategoryDTO expenseCategory);
+        Task Update(ExpenseCategoryDTO expenseCategory);
+        Task Delete(ExpenseCategoryDTO expenseCategory);
     }
 }
