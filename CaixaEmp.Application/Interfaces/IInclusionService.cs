f56079c17b80b3a587b16e56e86c0a9348718e4d
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-23 20:56:01 -0300

    Implementação da interface IWithdrawalService

--- CaixaEmp.Application/Interfaces/IInclusionService.cs
+++ CaixaEmp.Application/Interfaces/IInclusionService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CaixaEmp.Application.DTOs;
+using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace CaixaEmp.Application.Interfaces
 {
     public interface IInclusionService
     {
+        Task<IEnumerable<InclusionDTO>> GetAllInclusionAsync();
+        Task<InclusionDTO> GetInclusionById(int? id);
+        Task<IEnumerable<InclusionDTO>> GetInclusionByExpense(int expenseId);
+        Task<IEnumerable<InclusionDTO>> GetInclusionByEmplooyer(int emplooyerId);
+        Task<IEnumerable<InclusionDTO>> GetInclusionByPeriod(DateTime beginDate, DateTime endDate);
+        Task<IEnumerable<InclusionDTO>> GetInclusionByPriority(string priority);
+        Task<IEnumerable<InclusionDTO>> GetInclusionAproved(bool status);
+        Task<IEnumerable<InclusionDTO>> GetInclusionReproved(bool status);
+
+        Task Create(InclusionDTO inclusion);
+        Task Update(InclusionDTO inclusion);
+        Task Delete(InclusionDTO inclusion);
     }
 }
