9028ba71b172ae70db59c9320449ff2b991ce098
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-23 20:54:36 -0300

    Implementação das interfaces IExpenseService e IWithdrawalService

--- CaixaEmp.Application/Interfaces/IExpenseService.cs
+++ CaixaEmp.Application/Interfaces/IExpenseService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CaixaEmp.Application.DTOs;
+using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,13 @@ namespace CaixaEmp.Application.Interfaces
 {
     public interface IExpenseService
     {
+        Task<IEnumerable<ExpenseDTO>> GetAllExpenseAsync();
+        Task<ExpenseDTO> GetExpenseById(int? id);
+        Task<IEnumerable<ExpenseDTO>> GetExpenseByEmplooyer(int id);
+        Task<ExpenseDTO> GetExpenseCategory(int? id);
+
+        Task Create(ExpenseDTO expense);
+        Task Update(ExpenseDTO expense);
+        Task Delete(ExpenseDTO expense);
     }
 }
--- CaixaEmp.Application/Interfaces/IWithdrawalService.cs
+++ CaixaEmp.Application/Interfaces/IWithdrawalService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CaixaEmp.Application.DTOs;
+using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace CaixaEmp.Application.Interfaces
 {
     public interface IWithdrawalService
     {
+        Task<IEnumerable<WithdrawalDTO>> GetAllWithdrawalAsync();
+        Task<WithdrawalDTO> GetWithdrawalById(int? id);
+        Task<IEnumerable<WithdrawalDTO>> GetWithdrawalByExpense(int expenseId);
+        Task<IEnumerable<WithdrawalDTO>> GetWithdrawalByEmplooyer(int emplooyerId);
+        Task<IEnumerable<WithdrawalDTO>> GetWithdrawalByPeriod(DateTime beginDate, DateTime endDate);
+        Task<IEnumerable<WithdrawalDTO>> GetWithdrawalByPriority(string priority);
+        Task<IEnumerable<WithdrawalDTO>> GetWithdrawalAproved(bool status);
+        Task<IEnumerable<WithdrawalDTO>> GetInclusionReproved(bool status);
+
+        Task Create(WithdrawalDTO withdrawal);
+        Task Update(WithdrawalDTO withdrawal);
+        Task Delete(WithdrawalDTO withdrawal);
     }
 }
