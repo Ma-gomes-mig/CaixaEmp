c915a97349ed849f912692d688b6a3f98f95556c
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-23 20:48:27 -0300

    Implementação da interface IEmplooyerService

--- CaixaEmp.Application/Interfaces/IEmplooyerService.cs
+++ CaixaEmp.Application/Interfaces/IEmplooyerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CaixaEmp.Application.DTOs;
+using CaixaEmp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace CaixaEmp.Application.Interfaces
 {
     public interface IEmplooyerService
     {
+        Task<IEnumerable<EmplooyerDTO>> GetAllEmplooyerAsync();
+        Task<EmplooyerDTO> GetEmplooyerByIdAsync(int? id);
+        Task<EmplooyerDTO> GetEmplooyerByName(string name);
+        Task<EmplooyerDTO> Create(Emplooyer emplooyer);
+        Task<EmplooyerDTO> Update(Emplooyer emplooyer);
+        Task<EmplooyerDTO> Delete(Emplooyer emplooyer);
     }
 }
