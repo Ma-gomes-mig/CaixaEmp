4e96fc2133c31ef01cbafbb27302d0d58df308df
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-24 20:16:59 -0300

    Declaração da classe BalanceService

--- CaixaEmp.Application/Services/BalanceService.cs
+++ CaixaEmp.Application/Services/BalanceService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using CaixaEmp.Application.DTOs;
+using CaixaEmp.Application.Interfaces;
+using CaixaEmp.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,21 @@ using System.Threading.Tasks;
 
 namespace CaixaEmp.Application.Services
 {
-    public class BalanceService
+    public class BalanceService : IBalanceService
     {
+        private IBalanceRepository _balanceRepository;
+        private readonly IMapper _mapper;
+
+        public BalanceService(IMapper mapper, IBalanceRepository balanceRepository)
+        {
+            _balanceRepository = balanceRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<BalanceDTO>> GetAllBalanceAsync()
+        {
+            var balanceEntity = await _balanceRepository.GetAllBalanceAsync();
+            return _mapper.Map<IEnumerable<BalanceDTO>>(balanceEntity);
+        }
     }
 }
