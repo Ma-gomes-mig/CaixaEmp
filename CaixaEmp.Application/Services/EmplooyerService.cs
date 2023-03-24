1dce211dd539898dcc136cf5b650f4ef157a9458
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-24 20:20:33 -0300

    Injeção de dependencias IEmplooyerRepository e IMapper e criação do contrutor EmplooyerService

--- CaixaEmp.Application/Services/EmplooyerService.cs
+++ CaixaEmp.Application/Services/EmplooyerService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using CaixaEmp.Application.Interfaces;
+using CaixaEmp.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace CaixaEmp.Application.Services
 {
-    public class EmplooyerService
+    public class EmplooyerService : IEmplooyerService
     {
+        private IEmplooyerRepository _emplooyerRepository;
+        private readonly IMapper _mapper;
+        public EmplooyerService(IEmplooyerRepository emplooyerRepository, IMapper mapper)
+        {
+            _emplooyerRepository = emplooyerRepository;
+            _mapper = mapper;
+        }
+
     }
 }
