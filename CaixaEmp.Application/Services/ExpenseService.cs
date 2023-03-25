f4350c871eea4d185f8d29f5adf31d7fab8e3388
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-25 17:32:19 -0300

    Declaração do método GetAllExpenseAsync()

--- CaixaEmp.Application/Services/ExpenseService.cs
+++ CaixaEmp.Application/Services/ExpenseService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using CaixaEmp.Application.DTOs;
+using CaixaEmp.Application.Interfaces;
+using CaixaEmp.Domain.Interfaces;
+using CaixaEmp.Infra.Data.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,50 @@ using System.Threading.Tasks;
 
 namespace CaixaEmp.Application.Services
 {
-    public class ExpenseService
+    public class ExpenseService : IExpenseService
     {
+        private IExpenseRepository _expenseRepository;
+        private readonly IMapper _mapper;
+        public ExpenseService( IExpenseRepository expenseRepository, IMapper mapper)
+        {
+            _expenseRepository = expenseRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ExpenseDTO>> GetAllExpenseAsync()
+        {
+            var expense = await _expenseRepository.GetAllExpenseAsync();
+            return _mapper.Map<IEnumerable<ExpenseDTO>>(expense);
+        }
+
+        public Task<ExpenseDTO> GetExpenseById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ExpenseDTO>> GetExpenseByEmplooyer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExpenseDTO> GetExpenseCategory(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(ExpenseDTO expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(ExpenseDTO expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(ExpenseDTO expense)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
