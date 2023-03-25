fd791a52ebb5f1a72628522be699025baa9db5aa
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-24 21:02:32 -0300

    Declaração dos métodos GetAllCategoryAsync e GetCategoryByIdAsync

---
+++ CaixaEmp.Application/Services/ExpenseCategoryService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using CaixaEmp.Application.DTOs;
+using CaixaEmp.Application.Interfaces;
+using CaixaEmp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Application.Services
+{
+    public class ExpenseCategoryService : IExpenseCategoryService
+    {
+        private IExpenseCategoryRepository _expenseCategoryRepository;
+        private readonly IMapper _mapper;
+        public ExpenseCategoryService(IExpenseCategoryRepository expenseRepository, IMapper mapper)
+        {
+            _expenseCategoryRepository = expenseRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ExpenseCategoryDTO>> GetAllCategoryAsync()
+        {
+            var categoryEntity = await _expenseCategoryRepository.GetAllCategoryAsync();
+            return _mapper.Map<IEnumerable<ExpenseCategoryDTO>>(categoryEntity);
+        }
+
+        public Task<IEnumerable<ExpenseCategoryDTO>> GetCategoryByExpense(int expenseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ExpenseCategoryDTO> GetCategoryByIdAsync(int? Id)
+        {
+            var categoryEntity = await _expenseCategoryRepository.GetCategoryByIdAsync(Id);
+            return _mapper.Map<ExpenseCategoryDTO>(categoryEntity);
+        }
+
+        public Task Create(ExpenseCategoryDTO expenseCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(ExpenseCategoryDTO expenseCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(ExpenseCategoryDTO expenseCategory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
