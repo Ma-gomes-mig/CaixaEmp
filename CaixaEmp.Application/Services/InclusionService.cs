02fbe4becbcd97e47377e6546fbb54800a0b966b
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-26 13:32:02 -0300

    Implementação da classe InclusionService

--- CaixaEmp.Application/Services/InclusionService.cs
+++ CaixaEmp.Application/Services/InclusionService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using CaixaEmp.Application.DTOs;
+using CaixaEmp.Application.Interfaces;
+using CaixaEmp.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,69 @@ using System.Threading.Tasks;
 
 namespace CaixaEmp.Application.Services
 {
-    public class InclusionService
+    public class InclusionService : IInclusionService
     {
+        private IInclusionRepository _inclusionRepository;
+        private readonly IMapper _mapper;
+        public InclusionService(IInclusionRepository inclusionRepository, IMapper mapper)
+        {
+            _inclusionRepository = inclusionRepository;
+            _mapper = mapper;
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetAllInclusionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InclusionDTO> GetInclusionById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetInclusionByExpense(int expenseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetInclusionByEmplooyer(int emplooyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetInclusionByPeriod(DateTime beginDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetInclusionByPriority(string priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetInclusionAproved(bool status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InclusionDTO>> GetInclusionReproved(bool status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(InclusionDTO inclusion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(InclusionDTO inclusion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(InclusionDTO inclusion)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
