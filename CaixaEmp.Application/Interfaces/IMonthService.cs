c3bb11e05c58b9c2821666bf6bae5cc8fc089ef4
Matheus Gomes <matheus.gom23@yahoo.com>
2023-04-04 21:33:15 -0300

    Criação e declaração da Interface IMonthService

---
+++ CaixaEmp.Application/Interfaces/IMonthService.cs
@@ -0,0 +1,15 @@
+﻿using CaixaEmp.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEmp.Application.Interfaces
+{
+    public interface IMonthService
+    {
+        Task <IEnumerable<MonthDTO>> GetAllMonthAsync();
+        Task<MonthDTO> GetMonthByIdAsync(int id);
+    }
+}
